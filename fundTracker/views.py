cb8ac3696468f5d2b36921727c3cdc7aa351ae48
gp1047 <gp1047@wildcats.unh.edu>
2016-11-15 19:06:46 -0500

    Template changes

--- fundTracker/views.py
+++ fundTracker/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
 # Create your views here.
+def deposit(request):
+	return render(request,"fundTracker/deposit.html",{})
--- settings/views.py
+++ settings/views.py
@@ -2,3 +2,9 @@ from django.shortcuts import render
 
 def home(request):
 	return render(request, 'home1.html', {})
+	
+def piggybank(request):
+	return render(request, 'piggybank.html', {})
+	
+def piggybankFundAccount(request):
+	return render(request, 'piggybankFundAccount.html', {})
