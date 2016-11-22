92a2bec0205b804a6356efdd9d24fc18986e49a5
carpepraedam <jax472unh@gmail.com>
2016-11-22 08:59:04 -0500

    adding persistent data support to fundTracker/trackFund

--- fundTracker/urls.py
+++ fundTracker/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
+    url(r'fund_account/', views.fund_account, name="fund_account"),
 	url(r'^deposit/', views.deposit, name="deposit"),
 	url(r'^trackFund/', views.trackFund, name="trackFund"),
 	url(r'^history/', views.history, name="history"),
--- fundTracker/views.py
+++ fundTracker/views.py
@@ -1,11 +1,20 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
+from .models import Fund
 
 # Create your views here.
 def deposit(request):
 	return render(request,"fundTracker/deposit.html",{})
 	
 def trackFund(request):
-	return render(request,"fundTracker/trackFund.html",{})
-	
+    u = request.user
+    f = Fund.objects.get(owner=u)
+    t = f.transaction_set.all()
+    return render(request,"fundTracker/trackFund.html",{'fund':f, 'transaction_list':t})
+
 def history(request):
 	return render(request,"fundTracker/history.html",{})
+
+def fund_account(request):
+    u = request.user
+    return render(request, 'fundTracker/fund_account.html', {})
